fecf4b12520a46320943e3a6471a081d2059bc64
Bánki Szabolcs <banki.szabolcs@gmail.com>
2023-11-19 13:50:15 +0100

    Add project files.

---
+++ FileHandlingBasic/TextFileFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileHandlingBasic
+{
+    public class TextFileFunctions
+    {
+        private readonly string _rootPath = AppDomain.CurrentDomain.BaseDirectory;
+        private string _fileName = "TextFileExample1.txt";
+
+        public TextFileFunctions(string fileName)
+        {
+            this._fileName = fileName;
+        }
+
+        public void WriteTextToFile(string[] lines)
+        {
+            using StreamWriter outputFile = new StreamWriter(Path.Combine(this._rootPath, _fileName), true);
+            foreach (var line in lines)
+            {
+                outputFile.WriteLine(line);
+            }
+        }
+
+        public string ReadFile()
+        {
+            using StreamReader inputStreamReader = new StreamReader(Path.Combine(this._rootPath, this._fileName));
+            string content = inputStreamReader.ReadToEnd();
+            return content;
+        }
+    }
+}
+
+//Amint lefutott ez a kód, a program megszabadul tőle automatikusan. Nem kell meghívni az outputFile.Close() -t
+//vagy outputFile.Dispose()
---
+++ PlayingWithFiles/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FileHandlingBasic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.IO;
+
+namespace PlayingWithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                WriteHeadingToScreen();
+                Console.WriteLine("Please enter the name of the file");
+                string fileName = Console.ReadLine();
+                string[] lines = new string[3];
+                TextFileFunctions textFileFunctions = new TextFileFunctions(fileName+".txt");
+                int counter = 0;
+
+                do
+                {
+                    Console.WriteLine($"Please add {(counter == 0 ? "a line" : "another line")} to the file:{fileName}.txt");
+                    lines[counter] = Console.ReadLine();
+                    counter++;
+                } while (counter<3);
+
+                textFileFunctions.WriteTextToFile(lines);
+                Console.Clear();
+                WriteHeadingToScreen();
+                Console.Write(textFileFunctions.ReadFile());
+                Console.Read();
+            }
+            catch (IOException e)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+        }
+        
+        private static void WriteHeadingToScreen()
+        {
+            Console.WriteLine("Basic .NET Cross Platform File Handling");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
